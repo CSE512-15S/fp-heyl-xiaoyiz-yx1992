aaf16b108e30fc899fe95c89f676811e488b8cf7
yangxinuw <yangxin199207@gmail.com>
2015-06-05 16:39:02 -0700

    code

--- split.py
+++ split.py
@@ -4,16 +4,53 @@ Created on Wed Jun 03 11:58:44 2015
 
 @author: yx1992
 """
-
+from string import whitespace
 import csv
-with open('.\\Data\\Boston.csv', 'rb') as csvfile:
+
+fout=open("sample.csv",'w')
+header="wordlist,countlist,maxlike,totallike,avelike\n"
+fout.write(header)
+with open('.\\Data\\total.csv', 'rb') as csvfile:
+    wordlist=[]
+    countlist=[]
+    maxlike=[]
+    totallike=[]
+    avelike=[]
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         s=''
         for i in range(4,len(row)-1):
             s+=row[i]
-            print s.encode
             #s=s[1:len(s)-1]
-            lines=s.split("Tag:")
+        #print s
+        s=s[2:len(s)-2]
+        s=''.join(s.split())
+        #print s
+        lines=s.split("Tag:")
+        #print len(lines)
         #print lines
-        print lines
+        if row[3]=='Number of Likes':
+            continue
+        like=int(row[3])
+        for i in range(1,len(lines)):
+            if lines[i] not in wordlist:
+                wordlist.append(lines[i])
+                countlist.append(1)
+                
+                maxlike.append(like)
+                totallike.append(like)
+                avelike.append(like)
+            else:
+                j=(k for k,v in enumerate(wordlist) if v==lines[i]).next()
+                countlist[j]+=1
+                if like> maxlike[j]:
+                    maxlike[j]=like
+                totallike[j]+=like
+                avelike[j]=totallike[j]/countlist[j]
+                
+    for i in range(len(wordlist)):
+        line=wordlist[i]+","+str(countlist[i])+","+str(maxlike[i])+","+str(totallike[i])+","+str(avelike[i])+"\n"
+        fout.write(line)    
+
+fout.close()
+        
