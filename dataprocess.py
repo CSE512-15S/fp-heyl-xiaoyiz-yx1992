36dd723b80f3551ef9d98e6009d09d5068083af0
yangxinuw <yangxin199207@gmail.com>
2015-06-07 22:51:54 -0700

    code

--- dataprocess.py
+++ dataprocess.py
@@ -13,47 +13,51 @@ Created on Wed Jun 03 11:58:44 2015
 """
 from string import whitespace
 import csv
+import os
+from os import walk
 
-filename="Seattle"
-openname=".\\Data\\"+filename+".csv"
-outname=filename+"_resample.csv"
-fout=open(outname,'w')
-header="score,countlist,avelike,img1,img2\n"
-fout.write(header)
-with open(openname, 'rb') as csvfile:
-    filterlist=[]
-    countlist=[]
-    totallike=[]
-    avelike=[]
-    linklist=[[]]
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in spamreader:
-        filtername=row[2]
-        if row[3]=='Number of Likes':
-            continue
-        like=int(row[3])
+for root, dirs, files in os.walk('./Data'):
+    for filename in files:
 
-        if filtername not in filterlist:
-            filterlist.append(filtername)
-            countlist.append(1)
+        if(filename!="total.csv"):
+            outname="Resample"+filename
+            fout=open(outname,'w')
+            header="score,countlist,avelike,img1,img2\n"
+            fout.write(header)
+            with open(openname, 'rb') as csvfile:
+                filterlist=[]
+                countlist=[]
+                totallike=[]
+                avelike=[]
+                linklist=[[]]
+                spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+                for row in spamreader:
+                    filtername=row[2]
+                    if row[3]=='Number of Likes':
+                        continue
+                    like=int(row[3])
             
-            totallike.append(like)
-            avelike.append(like)
-            linklist.append([row[len(row)-1]])
-        else:
-            j=(k for k,v in enumerate(filterlist) if v==filtername).next()
-            countlist[j]+=1
-            totallike[j]+=like
-            avelike[j]=totallike[j]/countlist[j]
-            linklist[j].append(row[len(row)-1])
+                    if filtername not in filterlist:
+                        filterlist.append(filtername)
+                        countlist.append(1)
+                        
+                        totallike.append(like)
+                        avelike.append(like)
+                        linklist.append([row[len(row)-1]])
+                    else:
+                        j=(k for k,v in enumerate(filterlist) if v==filtername).next()
+                        countlist[j]+=1
+                        totallike[j]+=like
+                        avelike[j]=totallike[j]/countlist[j]
+                        linklist[j].append(row[len(row)-1])
+                        
+                zipped=zip(filterlist,countlist,avelike,linklist)
+                zipped.sort(key = lambda t: t[1],reverse=True)     
+                f,c,a,l=zip(*zipped)
+                for i in range(len(f)):
+                    if c[i]>=4:
+                        line=f[i]+","+str(c[i])+","+str(a[i])+","+str(l[i][0])+","+str(l[i][1])+","+str(l[i][2])+","+str(l[i][3])+"\n"
+                        fout.write(line)    
             
-    zipped=zip(filterlist,countlist,avelike,linklist)
-    zipped.sort(key = lambda t: t[1],reverse=True)     
-    f,c,a,l=zip(*zipped)
-    for i in range(len(f)):
-        if c[i]>=4:
-            line=f[i]+","+str(c[i])+","+str(a[i])+","+str(l[i][0])+","+str(l[i][1])+","+str(l[i][2])+","+str(l[i][3])+"\n"
-            fout.write(line)    
-
-fout.close()
+            fout.close()
         
